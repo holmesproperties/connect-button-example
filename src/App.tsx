8f2f0a8b259b0cc70255dbcdd39f3a1e7b1f3e2d
Eiman <eabdelmoneim@gmail.com>
2024-09-17 14:18:18 -0500

    removed ethers

--- src/App.tsx
+++ src/App.tsx
@@ -1,541 +1,12 @@
-import { createThirdwebClient } from "thirdweb";
+import { createThirdwebClient, defineChain, getContract } from "thirdweb";
+import { createWallet, walletConnect, ecosystemWallet, inAppWallet } from "thirdweb/wallets";
 import { ethers5Adapter } from 'thirdweb/adapters/ethers5';
 import { ethers } from 'ethers';
 import "./App.css";
-import { ConnectButton, useActiveAccount } from "thirdweb/react";
+import { ConnectButton, useActiveAccount, useActiveWallet, useConnectModal } from "thirdweb/react";
+import { getAllAdmins } from "thirdweb/extensions/erc4337";
+import { useState } from "react";
 
-const koshaNFTFactoryAbi = [
-  {
-    "type": "constructor",
-    "name": "",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "_addressRegistry",
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "error",
-    "name": "AlreadyInitialized",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "ApproverUnauthorized",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "EmptyAssetProvided",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "EmptyURIProvided",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "EvolverUnauthorized",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "InsufficientSpinoffRoyaltyMinValue",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "InvalidCurrentTokenOwner",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "InvalidOwner",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "InvalidOwnerNotApproved",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "NotApprovedForSpinoff",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "NotEnoughNFTsForDistribution",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "NotValidPaymentToken",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "NotValidRoyaltyPayToken",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "OwnedUnauthorized",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "OwnedZeroAddressProvided",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "Pausable_NotPaused",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "Pausable_Paused",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "PayTokenNotApplied",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "PriceCanNotBeZero",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "ReceiverLengthCanNotBeZero",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "Registry__TokenNotRegistered",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "SpinoffRequestRequiredCreatorApproval",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "SpinoffRequestRequiredCreatorValidation",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "SpinoffUnauthorized",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "ZeroAddressProvided",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "error",
-    "name": "ZeroExpirationTimestamp",
-    "inputs": [],
-    "outputs": []
-  },
-  {
-    "type": "event",
-    "name": "EndKoshaProxyDeployed",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "sender",
-        "indexed": true,
-        "internalType": "address"
-      },
-      {
-        "type": "address",
-        "name": "deployedProxy",
-        "indexed": true,
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "anonymous": false
-  },
-  {
-    "type": "event",
-    "name": "OwnershipTransferred",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "user",
-        "indexed": true,
-        "internalType": "address"
-      },
-      {
-        "type": "address",
-        "name": "newOwner",
-        "indexed": true,
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "anonymous": false
-  },
-  {
-    "type": "event",
-    "name": "Paused",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "account",
-        "indexed": false,
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "anonymous": false
-  },
-  {
-    "type": "event",
-    "name": "StartKoshaProxyDeployed",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "sender",
-        "indexed": true,
-        "internalType": "address"
-      },
-      {
-        "type": "address",
-        "name": "deployedProxy",
-        "indexed": true,
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "anonymous": false
-  },
-  {
-    "type": "event",
-    "name": "Unpaused",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "account",
-        "indexed": false,
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "anonymous": false
-  },
-  {
-    "type": "function",
-    "name": "addressRegistry",
-    "inputs": [],
-    "outputs": [
-      {
-        "type": "address",
-        "name": "",
-        "internalType": "contract IAddressRegistry"
-      }
-    ],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "deployEndKoshaProxy",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "_creator",
-        "internalType": "address"
-      },
-      {
-        "type": "uint256",
-        "name": "_amount",
-        "internalType": "uint256"
-      },
-      {
-        "type": "tuple",
-        "name": "_metadata",
-        "components": [
-          {
-            "type": "string",
-            "name": "tokenURI",
-            "internalType": "string"
-          },
-          {
-            "type": "string",
-            "name": "tokenAsset",
-            "internalType": "string"
-          },
-          {
-            "type": "string",
-            "name": "licenseURI",
-            "internalType": "string"
-          }
-        ],
-        "internalType": "struct TokenMetadata"
-      },
-      {
-        "type": "tuple",
-        "name": "_principalRoyaltyInfo",
-        "components": [
-          {
-            "type": "address",
-            "name": "receiver",
-            "internalType": "address"
-          },
-          {
-            "type": "uint24",
-            "name": "royaltyShare",
-            "internalType": "uint24"
-          },
-          {
-            "type": "address",
-            "name": "payToken",
-            "internalType": "address"
-          },
-          {
-            "type": "uint256",
-            "name": "minValue",
-            "internalType": "uint256"
-          }
-        ],
-        "internalType": "struct PrincipalRoyaltyInfo"
-      }
-    ],
-    "outputs": [
-      {
-        "type": "address",
-        "name": "",
-        "internalType": "address"
-      }
-    ],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "deployKoshaProxy",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "_creator",
-        "internalType": "address"
-      },
-      {
-        "type": "tuple",
-        "name": "_metadata",
-        "components": [
-          {
-            "type": "string",
-            "name": "tokenURI",
-            "internalType": "string"
-          },
-          {
-            "type": "string",
-            "name": "tokenAsset",
-            "internalType": "string"
-          },
-          {
-            "type": "string",
-            "name": "licenseURI",
-            "internalType": "string"
-          }
-        ],
-        "internalType": "struct TokenMetadata"
-      },
-      {
-        "type": "tuple",
-        "name": "_principalRoyaltyInfo",
-        "components": [
-          {
-            "type": "address",
-            "name": "receiver",
-            "internalType": "address"
-          },
-          {
-            "type": "uint24",
-            "name": "royaltyShare",
-            "internalType": "uint24"
-          },
-          {
-            "type": "address",
-            "name": "payToken",
-            "internalType": "address"
-          },
-          {
-            "type": "uint256",
-            "name": "minValue",
-            "internalType": "uint256"
-          }
-        ],
-        "internalType": "struct PrincipalRoyaltyInfo"
-      },
-      {
-        "type": "tuple",
-        "name": "_spinoffRoyaltyInfo",
-        "components": [
-          {
-            "type": "uint24",
-            "name": "royaltyShare",
-            "internalType": "uint24"
-          },
-          {
-            "type": "address",
-            "name": "payToken",
-            "internalType": "address"
-          },
-          {
-            "type": "uint256",
-            "name": "minValue",
-            "internalType": "uint256"
-          }
-        ],
-        "internalType": "struct SpinoffRoyaltyInfo"
-      },
-      {
-        "type": "uint8",
-        "name": "_stateRequestApproval",
-        "internalType": "enum KoshaNFT.StateRequestApproval"
-      }
-    ],
-    "outputs": [
-      {
-        "type": "address",
-        "name": "",
-        "internalType": "address"
-      }
-    ],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "exists",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "",
-        "internalType": "address"
-      }
-    ],
-    "outputs": [
-      {
-        "type": "bool",
-        "name": "",
-        "internalType": "bool"
-      }
-    ],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "owner",
-    "inputs": [],
-    "outputs": [
-      {
-        "type": "address",
-        "name": "",
-        "internalType": "address"
-      }
-    ],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "pause",
-    "inputs": [],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "paused",
-    "inputs": [],
-    "outputs": [
-      {
-        "type": "bool",
-        "name": "",
-        "internalType": "bool"
-      }
-    ],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "renounceOwnership",
-    "inputs": [],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "transferOwnership",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "newOwner",
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "payable"
-  },
-  {
-    "type": "function",
-    "name": "unpause",
-    "inputs": [],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "updateAddressRegistry",
-    "inputs": [
-      {
-        "type": "address",
-        "name": "_addressRegistry",
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  }
-];
 
 const client = createThirdwebClient({
   // If not using Vite, then use `process.env.NEXT_PUBLIC_CLIENT_ID`
@@ -545,55 +16,69 @@ const client = createThirdwebClient({
 
 function App() {
   const  account = useActiveAccount();
+  const activeWallet = useActiveWallet();
+  const { connect, isConnecting } = useConnectModal();
+  const [isSmartAccountConnected, setSmartAccountConnected] = useState(false);
 
-  const handleEthersSigner = async () => {
-    if (!account) {
-      return;
+  const wallets = [ 
+    ecosystemWallet(
+      "ecosystem.xai-connect", {
+      partnerId: "689cdf15-34a3-4356-b0ee-abc5f160cd0f",
+    }),
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  walletConnect()
+  ];
+
+  const recommendedWallets = [
+    ecosystemWallet("ecosystem.xai-connect", {
+      partnerId: "689cdf15-34a3-4356-b0ee-abc5f160cd0f",
+    }),
+  ]
+
+  async function handleConnect() {
+    const wallet = await connect({
+      client, 
+      // accountAbstraction:{x
+      //   chain: defineChain(37714555429),
+      //   sponsorGas: true
+      // },
+      wallets: 
+        [ecosystemWallet("ecosystem.xai-connect", {
+          partnerId: "689cdf15-34a3-4356-b0ee-abc5f160cd0f",
+        }),
+        //   createWallet("io.metamask"),
+        // createWallet("com.coinbase.wallet"),
+        // walletConnect()
+        ],
+      setActive: true
+     }); // opens the connect modal
+    console.log("connected to", wallet);
+     setSmartAccountConnected(true);
+
     }
-    const ethersSigner = ethers5Adapter.signer.toEthers({
+
+    async function handleConnectSmartAccount() {
+      if (!account) {
+        return;
+
+        
+      }
+
+    const contract = getContract({
       client,
-      chain: { id: 80002, rpc: 'https://rpc-amoy.polygon.technology/' },
-      account: account,
+      chain: defineChain(37714555429),
+      address: account?.address || "",
     });
-  
-    const signer = await ethersSigner;
-    const fee = (await signer.getGasPrice()).toNumber();
-    console.log(fee);
+    console.log("contract", contract);
 
-    const koshaNFTFactory = new ethers.Contract("0x489bc07071CdDd476F4AF388197dc2f55d940cD0", koshaNFTFactoryAbi, signer);
-    console.log(koshaNFTFactory.address);
+    const result = getAllAdmins({
+      contract,
+    });
 
-    const tx = await koshaNFTFactory.owner();
-    console.log(tx);
-
-    const metadata = {
-      tokenURI: "https://gateway.pinata.cloud/ipfs/QmZo1D6z3bW8Q5y7Z4ZB2v2n2r5Zo5Z8zL9q6Z2z",
-      tokenAsset: "https://gateway.pinata.cloud/ipfs/QmZo1D6z3bW8Q5y7Z4ZB2v2n2r5Zo5Z8zL9q6Z2z",
-      licenseURI: "https://gateway.pinata.cloud/ipfs/QmZo1D6z3bW8Q5y7Z4ZB2v2n2r5Zo5Z8zL9q6Z2z"
+    console.log(result);
     }
 
-    const principalRoyaltyInfo = {
-      receiver: "0x489bc07071CdDd476F4AF388197dc2f55d940cD0",
-      royaltyShare: 100,
-      payToken: "0x0000000000000000000000000000000000000000",
-      minValue: 0
-    }
-
-    console.log("deploying proxy");
-
-    const tx2 = await koshaNFTFactory.deployEndKoshaProxy(
-      await signer.getAddress(),
-      1,
-      metadata,
-      principalRoyaltyInfo,
-      { gasPrice: fee, gasLimit: 10000000 },
-    );
-
-    tx2.wait().then((receipt: any) => {
-      console.log(receipt);
-    });
-  
-  }
   return (
     <div>
       <div style={{ marginBottom: "12px", maxWidth: "420px" }}>
@@ -607,18 +92,20 @@ function App() {
           Playground
         </a>
       </div>
-      <ConnectButton client={client} />
+      <ConnectButton client={client} wallets={wallets} recommendedWallets={recommendedWallets}/>
       <div>
         {account ? (
           <div>
             <div>Account: {account.address}</div>
-            <button onClick={() => 
-              { 
-                handleEthersSigner();
-              }}>Do Ethers</button>
+            <button onClick={handleConnect} disabled={isConnecting}>Connect Wallet</button>
           </div>
         ) : (
           <div>Not connected</div>
+        )}
+        {isSmartAccountConnected ?(
+          <button onClick={handleConnectSmartAccount}>Get All Admins</button>
+        ) : (
+          <div/>
         )}
       </div>
     </div>
